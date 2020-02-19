4e52ced8bf0bda9f95ec247958c8828248545da5
b45i <amalshajan2011@gmail.com>
2020-02-19 11:37:44 +0530

    formatting

--- orderStrings.js
+++ orderStrings.js
@@ -4,8 +4,8 @@
  * https://www.codewars.com/kata/55c45be3b2079eccff00010f/train/javascript
  */
 
-function order(words){
+function order(words) {
     return words.split(' ').sort((a, b) => +a.match(/\d/g) - +b.match(/\d/g)).join(' ');
-  }
-  
-  order("is2 Thi1s T4est 3a")
+}
+
+order("is2 Thi1s T4est 3a")
